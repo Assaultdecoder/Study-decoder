c294a6c181dfe1d418eec1a1436f6ddabfcb6ff3
Assaultdecoder <anujsinghy066@gmail.com>
2025-07-06 22:44:02 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,39 @@
+window.onload = function () {
+  showPopup();
+  loadBatches();
+};
 
+function showPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function loadBatches() {
+  const container = document.getElementById("batchContainer");
+
+  Object.keys(DATA).forEach(batch => {
+    const bDiv = document.createElement("div");
+    bDiv.innerHTML = `<h2>${batch}</h2>`;
+    
+    Object.keys(DATA[batch]).forEach(subject => {
+      const sDiv = document.createElement("div");
+      sDiv.innerHTML = `<h3>➤ ${subject}</h3>`;
+
+      DATA[batch][subject].forEach(lecture => {
+        const lDiv = document.createElement("div");
+        lDiv.innerHTML = `
+          <p><strong>${lecture.title}</strong></p>
+          <iframe width="100%" height="315" src="${lecture.url}" frameborder="0" allowfullscreen></iframe>
+        `;
+        sDiv.appendChild(lDiv);
+      });
+
+      bDiv.appendChild(sDiv);
+    });
+
+    container.appendChild(bDiv);
+  });
+}
